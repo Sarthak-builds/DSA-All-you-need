dc7b05bb22f11d1a95b0361993f9eb254bcece7f
Sarthak-builds <sarthakshiroty04@gmail.com>
2026-03-23 19:08:03 +0530

    swap alternative

---
+++ Arrays/swapAlternate.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+
+void reverse(int arr[], int n) {
+     for( int i=0; i < n; i+=2) {
+         if( i+1 < n){
+         swap(arr[i], arr[i+1]);
+     }
+}
+}
+void printArray(int arr[], int n) {
+    for (int i =0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    int arr[6] = { 2, 7, 5, 9, -2, 15 };
+    int brr[5] = { 2, 7, 5, 9, -2 };
+    reverse(arr, 6);
+    reverse(brr, 5);
+    
+    printArray(arr, 6);
+    printArray(brr, 5);
+    
+    
+   return 0; 
+}
